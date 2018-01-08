3b6c4ae75d009c076383350c9338c0879ad16c0b
ldming <china-live@live.cn>
2018-01-09 00:57:22 +0800

    添加模块XCore.DisplayManagement（未完）

---
+++ src/XCore/XCore.DisplayManagement/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XCore.DisplayManagement.Theming;
+
+namespace XCore.DisplayManagement
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTheming(this IServiceCollection services)
+        {
+            services.AddScoped<IThemeManager, ThemeManager>();
+            return services;
+        }
+    }
+}
