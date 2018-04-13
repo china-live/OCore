a7b93db1984873ead6f0cc31340e3480b4e4df57
ldming <china-live@live.cn>
2018-04-14 03:11:13 +0800

    小的更新

--- src/Dome.Modules/XCore.Mvc.HelloWorld/Controllers/HomeController.cs
+++ src/Dome.Modules/XCore.Mvc.HelloWorld/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using XCore.Admin;
 
 namespace XCore.Mvc.HelloWorld.Controllers
 {
-    [Admin]
     public class HomeController : Controller
     {
         public IActionResult Index()
